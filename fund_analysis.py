be3357bd6f608aaffb8e183199ade731aebdb90b
WB514964 <ngoldberger@worldbank.org>
2018-01-03 17:41:54 -0500

    CHange

--- fund_analysis.py
+++ fund_analysis.py
@@ -511,7 +511,8 @@ class AnalysisTools:
             exposures[fund] = r2
         return exposures
 
-
+        asd
+        
 if __name__ == '__main__':
     dc = data_management.DataConnect(path='C:\\Users\\wb514964\\Code\\em-equity\\database\\', database='EMEQ.db')
     at = AnalysisTools()
